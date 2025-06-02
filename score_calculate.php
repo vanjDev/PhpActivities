be7caddfd36f13cd1d3bf1c6020214b18e525d04
vanjDev <143190477+vanjDev@users.noreply.github.com>
2025-06-02 17:23:14 +0800

    Update score_calculate.php

--- score_calculate.php
+++ score_calculate.php
@@ -1,1 +1,26 @@
+<?php
+function calculateAverage($scores) {
+    return array_sum($scores) / count($scores);
+}
 
+$name = $_POST['name'];
+
+$subjects = [
+    $_POST['subject1'] => $_POST['score1'],
+    $_POST['subject2'] => $_POST['score2'],
+    $_POST['subject3'] => $_POST['score3']
+];
+
+$scores = array_values($subjects);
+$average = calculateAverage($scores);
+
+// Output
+echo "<h2>Student Report</h2>";
+echo "Name: " . htmlspecialchars($name) . "<br><br>";
+
+foreach ($subjects as $subject => $score) {
+    echo "Subject: " . htmlspecialchars($subject) . " - Score: " . htmlspecialchars($score) . "<br>";
+}
+
+echo "<br><strong>Average Score:</strong> " . round($average, 2);
+?>
