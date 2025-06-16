621bce4f5bc8e69ce26f0fb602efa8fca70933a8
vanjDev <143190477+vanjDev@users.noreply.github.com>
2025-06-16 18:01:09 +0800

    Add files via upload

---
+++ June16/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+  <title>Short Stories</title>
+</head>
+<body>
+
+<ul>
+  <li><a href="story1.php">2021 Abu Dhabi Grand Prix</a></li>
+  <li><a href="story2.php">Juan Tamad</a></li>
+  <li><a href="story3.php">Aladdin</a></li>
+  <li><a href="story4.php">Beauty and The Beast</a></li>
+  <li><a href="story5.php">Story 5</a></li>
+</ul>
