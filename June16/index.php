bbcdf1048bbb0610e879ccb7ad57cde0c3e2eda4
vanjDev <143190477+vanjDev@users.noreply.github.com>
2025-06-16 17:23:14 +0800

    Create index.php

---
+++ June16/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 22px 150px;
+  text-decoration: none;
+  font-size: 22px;     /* Increased font size */
+  font-weight: bold;   /* Optional: makes text bolder */
+}
+
+
+li a:hover {
+  background-color: #111;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a href="story1.php">Story 1</a></li>
+  <li><a href="story2.php">Story 2</a></li>
+  <li><a href="story3.php">Story 3</a></li>
+  <li><a href="story4.php">Story 4</a></li>
+  <li><a href="story5.php">Story 5</a></li>
+</ul>
+
+</body>
+</html>
+
+
