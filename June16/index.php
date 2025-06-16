a983adc8054df62d5dda064caaa1ed43ab70f933
vanjDev <143190477+vanjDev@users.noreply.github.com>
2025-06-16 18:13:00 +0800

    Delete June16 directory

--- June16/index.php
+++
@@ -1,6 +0,0 @@
-<?php include("header.php"); ?>
-<div class="story">
-  <h2>Welcome to my Short Stories!</h2>
-  <p>Select a story you want above.</p>
-</div>
-<?php include("footer.php"); ?>
--- June16/story5.php
+++
@@ -1,7 +0,0 @@
-<?php include("header.php"); ?>
-<div class="story">
-  <h2>The Forest Whisper</h2>
-  <img src="images/story1.jpg" alt="Forest">
-  <p>Once upon a time, in a silent forest, a whisper traveled with the wind. It carried secrets from the trees to the creatures below. Only the brave could hear it...</p>
-</div>
-<?php include("footer.php"); ?>
